1976fab81451113a0764e9bca593090943461a74
parhat <parhat93@gmail.com>
2022-01-25 21:38:51 -0500

    Collection Reviewer are added.

--- collections-review/src/MapReview.java
+++ collections-review/src/MapReview.java
@@ -1,5 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapReview {
+
     public static void main(String[] args) {
 
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"Sergiu");
+        map.put(2,"Moses");
+        System.out.println(map.get(2));
+
+        //Find the first non-repeating char in a string
+        System.out.println(findFirstNonRepeating("Amazing"));
+    }
+
+    public static Character findFirstNonRepeating(String str){
+        Map<Character,Integer> map = new HashMap<>();
+        // for counting the chars
+        int count;
+        // counting chars and putting values in the hash map
+        for(Character ch:str.toCharArray()){
+            if(map.containsKey(ch)){
+                count=map.get(ch);
+                map.put(ch,count+1);
+            }else map.put(ch,1);
+        }
+        // starting from string, check if char count equals 1
+        for (Character ch:str.toCharArray()){
+            if(map.get(ch)==1) return ch;
+        }
+        return Character.MIN_VALUE;
     }
 }
--- collections-review/src/SetReview.java
+++ collections-review/src/SetReview.java
@@ -1,5 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetReview {
     public static void main(String[] args) {
 
+        //create a set
+        Set<Student> mySet = new HashSet<>();
+
+        // add element
+        mySet.add(new Student(7,"Tim"));
+        mySet.add(new Student(8,"Johny"));
+        mySet.add(new Student(9,"muhammad"));
+        mySet.add(new Student(9,"muhammad"));
+
+        System.out.println(mySet);
+
+        Set<Integer> numSet = new HashSet<>();
+        numSet.add(1);
+        numSet.add(2);
+        System.out.println(numSet);
+        System.out.println(numSet.add(2));
+
+        //Printing the Repeating char
+        System.out.println("first repeating: " + firstRepeatingChar("java developer"));
     }
+
+    public static Character firstRepeatingChar(String str){
+        //Create a hashmap
+        Set<Character> chars = new HashSet<>();
+
+        //Iteration return char if add returns false
+        for (Character ch:str.toCharArray()) if(!chars.add(ch)) return ch;
+        return null;
+    }
+
 }
